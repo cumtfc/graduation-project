bd3b75521e4385a6bf3a85ffce63af7b38655d99
fc <fc751590330@outlook.com>
2018-11-11 13:15:51 +0800

    init

--- 001_model_xgb.py
+++ 001_model_xgb.py
@@ -8,9 +8,10 @@ import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from imblearn.combine import SMOTEENN
+from win32timezone import now
+
 
 warnings.filterwarnings("ignore")
-
 
 
 def feat_select(train, test):
@@ -21,9 +22,7 @@ def feat_select(train, test):
     return features, target
 
 
-def xgbCV(train, test):
-    features, target = feat_select(train, test)
-
+def xgbCV(train, test, features, target):
     X = train[features]
     y = train[target]
     X_tes = test[features]
@@ -53,9 +52,7 @@ def xgbCV(train, test):
     return best_iter_num
 
 
-def sub(train, test, best_iter_num):
-    features, target = feat_select(train, test)
-
+def sub(train, test, features, target, best_iter_num):
     X = train[features]
     y = train[target]
     X_train_set = xgb.DMatrix(X, label=y, missing=np.nan)
@@ -84,28 +81,66 @@ def sub(train, test, best_iter_num):
     # sub[['instance_id', 'predicted_score']].to_csv('xgb_mean_%s.txt' % mean, sep=" ", index=False)
 
 
+def train():
+    sm = SMOTEENN()
+    train_data.replace(to_replace=np.nan, value=0, inplace=True)
+    train_data.replace(to_replace=-np.inf, value=0, inplace=True)
+    train_data.replace(to_replace=np.inf, value=0, inplace=True)
+    print("rebalance data:", now())
+    X_resampled, y_resampled = sm.fit_resample(train_data[features], train_data[target])
+    X_resampled = pd.DataFrame(X_resampled, columns=features)
+    y_resampled = pd.DataFrame(y_resampled, columns=target)
+    X_resampled['is_trade'] = y_resampled['is_trade']
+    del y_resampled
+    gc.collect()
+    print("starting CV:", now())
+    best_iter = xgbCV(X_resampled, test_data, features, target)
+    print('最佳迭代次数：', best_iter)
+    sub(train_data, test_data, features, target, best_iter)
+
+
+def count():
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+    for i in range(0, len(predictions)):
+        if predictions[i] == y_true.T[0][i]:
+            if predictions[i] == 1:
+                tp += 1
+            else:
+                tn += 1
+        else:
+            if predictions[i] == 1:
+                fp += 1
+            else:
+                fn += 1
+
+    print(tp, tn, fp, fn)
+
+
 if __name__ == "__main__":
     path = './data/'
 
     data = pd.read_csv(path + 'all_final_data_11-07-18-51.csv')
     data = data[data.is_trade.notnull()]
     train_data, test_data = train_test_split(data, test_size=0.25)
-    # best_iter = xgbCV(train_data, test_data)
-    # print('最佳迭代次数：', best_iter)
-    # sub(train_data, test_data, best_iter)
-
-    # gc.collect()
     features, target = feat_select(train_data, test_data)
+    # train()
+    gc.collect()
     bst = xgb.Booster({'nthread': 6})  # init model
     bst.load_model('001.model')  # load data
     X_test_set = xgb.DMatrix(test_data[features], missing=np.nan)
-    y_pred=bst.predict(X_test_set)
+    y_pred = bst.predict(X_test_set)
+    predictions = [round(x) for x in y_pred]
     y_true = test_data[target].values
+    count()
     # 输出模型的一些结果
     print("\n关于现在这个模型")
-    print("准确率 : %.4g" % metrics.accuracy_score(y_true, y_pred))
-    print("召回率:%.4f" % metrics.recall_score(y_true, y_pred, average='macro'))
-    print("召回率:%.4f" % metrics.f1_score(y_true, y_pred, average='weighted'))
+    print("准确率 : %.4g" % metrics.accuracy_score(y_true, predictions))
+    print("召回率:%.4f" % metrics.recall_score(y_true, predictions))
+    print("精度:%.4f" % metrics.precision_score(y_true, predictions))
+    print("F1:%.4f" % metrics.f1_score(y_true, predictions))
     print("AUC 得分 (训练集): %f" % 0.906732)
     print("AUC 得分 (测试集): %f" % 0.769061)
     # xgb.plot_importance(alg.get_booster())
